3242a28a442579d9518cd9262830c479045a7ccd
markwalkernz <markwalkernz@gmail.com>
2021-02-17 14:06:48 +0100

    Updated links to images

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,37 +1,37 @@
 var friendsList = [
 {
 "name": "Scott Summers",
-"photo": "https://upload.wikimedia.org/wikipedia/en/d/d9/Cyclops_%28Marvel_Comics_character%29.jpg",
+"photo": "https://upload.wikimedia.org/wikipedia/en/thumb/e/e9/Cyclops_%28Scott_Summers_circa_2019%29.png/250px-Cyclops_%28Scott_Summers_circa_2019%29.png",
 "scores" : ['2','1','5','5','1','1','2','5','4','1']
 },
 {
 "name": "Ororo Munroe",
-"photo": "https://upload.wikimedia.org/wikipedia/en/f/f9/X-Men_Storm_Main.png",
+"photo": "https://en.wikipedia.org/wiki/File:Storm_(Ororo_Munroe).png",
 "scores" : ['5','2','5','4','4','1','5','4','4','2']
 },
 {
 "name": "Anna Marie",
-"photo": "https://en.wikipedia.org/wiki/Rogue_(comics)#/media/File:Avengers_Rogue.jpg",
+"photo": "https://en.wikipedia.org/wiki/File:Rogue_(Marvel_Comics_character_-_circa_2018).png",
 "scores" : ['4','5','1','1','3','5','4','3','3','5']
 },
 {
 "name": "Piotr Rasputin",
-"photo": "https://upload.wikimedia.org/wikipedia/en/f/f8/AstonColossus.png",
+"photo": "https://upload.wikimedia.org/wikipedia/en/thumb/2/26/Colossus-AvX_Consequences.jpg/250px-Colossus-AvX_Consequences.jpg",
 "scores" : ['3','2','5','4','1','1','4','5','5','1']
 },
 {
 "name": "Logan Howlett",
-"photo": "https://upload.wikimedia.org/wikipedia/en/c/c8/Marvelwolverine.jpg",
+"photo": "https://upload.wikimedia.org/wikipedia/en/thumb/5/5d/Wolverine_%28James_%27Logan%27_Howlett%29.png/250px-Wolverine_%28James_%27Logan%27_Howlett%29.png",
 "scores" : ['5','5','1','1','3','5','2','2','2','1']
 },
 {
 "name": "Kurt Wagner",
-"photo": "https://upload.wikimedia.org/wikipedia/en/8/88/Nightcrawler.PNG",
+"photo": "https://upload.wikimedia.org/wikipedia/en/8/88/Nightcrawler.PNGhttps://upload.wikimedia.org/wikipedia/en/thumb/7/7b/Nightcrawler_%28Kurt_Wagner_circa_2018%29.png/250px-Nightcrawler_%28Kurt_Wagner_circa_2018%29.png",
 "scores" : ['3','4','1','2','3','2','5','5','5','3']
 },
 {
 "name": "Raven Darkholme",
-"photo": "https://upload.wikimedia.org/wikipedia/en/b/bb/Mystique11.png",
+"photo": "https://upload.wikimedia.org/wikipedia/en/thumb/6/68/Mystique_%28circa_2020%29.jpg/250px-Mystique_%28circa_2020%29.jpg",
 "scores" : ['2','5','1','3','5','5','3','1','1','3']
 }
 ]
